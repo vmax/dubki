ec36ff8cc718d889df12232ac718507282c242a7
Max Vorobev <vmax0770@gmail.com>
2015-06-19 13:20:17 +0300

    favorite station is now common between two fields

--- static/dubki.js
+++ static/dubki.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
     var to = $("#_to")[0];
     var from = $("#_from")[0];
-    
+
     var go1 = $("#go1")[0];
     var go2 = $("#go2")[0];
-    
-    if ($.jStorage.get('to-fav')) {
+
+    if ($.jStorage.get('fav-station')) {
         go1.disabled=false;
-        to.value = $.jStorage.get('to-fav');
+        to.value = $.jStorage.get('fav-station');
     }
-    
-    if ($.jStorage.get('from-fav')) {
+
+    if ($.jStorage.get('fav-station')) {
         go2.disabled=false;
-        from.value = $.jStorage.get('from-fav');
+        from.value = $.jStorage.get('fav-station');
     }
-    
+
     to.onchange = function(){
         go1.disabled=false;
-        $.jStorage.set('to-fav',to.value);
-        
+        $.jStorage.set('fav-station',to.value);
+
     }
     from.onchange = function(){
         go2.disabled=false;
-        $.jStorage.set('from-fav',from.value);
+        $.jStorage.set('fav-station',from.value);
     }
-    
+
 });
