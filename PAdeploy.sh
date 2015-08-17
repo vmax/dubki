a150868200ea9836384525b7b796790e25403dd5
Max Vorobev <vmax0770@gmail.com>
2015-08-17 22:19:30 +0300

    fixed the deployment script once again added Yandex.Metrika snippets

--- PAdeploy.sh
+++ PAdeploy.sh
@@ -7,9 +7,12 @@ rsync -R *.py .train_api_key templates/* static/* .pa_temp/
 # change directory
 cd .pa_temp
 # patch the files
+sed -e 's/.train_api_key/\/home\/dubki\/app\/.train_api_key/' route_train.py | tee route_train_PA.py
+sed -e 's/.bus_schedule/\/home\/app\/dubki\/.bus_schedule/' route_bus.py  | tee route_bus_PA.py
+mv route_train_PA.py route_train.py
+mv route_bus_PA.py route_bus.py
 
-sed -e 's/.train_api_key/\/home\/dubki\/app\/.train_api_key/' -i route_train.py
-sed -e 's/.bus_schedule/\/home\/app\/dubki\/.bus_schedule/' -i route_bus.py 
+
 
 # archive files
 tar -cjf ../dubki.tbz2 .
