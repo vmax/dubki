1aa83c4c9ad5a79b69f473f75ba604e22170c630
Max Vorobev <vmax0770@gmail.com>
2015-08-28 02:25:38 +0300

    fix deploy script

--- PAdeploy.sh
+++ PAdeploy.sh
@@ -7,8 +7,8 @@ rsync -R *.sh *.py .train_api_key templates/* static/* .pa_temp/
 # change directory
 cd .pa_temp
 # patch the files
-sed -e 's/.train_api_key/\/home\/dubki\/app\/.train_api_key/' route_train.py | tee route_train_PA.py
-sed -e 's/.bus_schedule/\/home\/app\/dubki\/.bus_schedule/' route_bus.py  | tee route_bus_PA.py
+sed -e 's/\.train_api_key/\/home\/dubki\/app\/\.train_api_key/' route_train.py | tee route_train_PA.py
+sed -e 's/\.bus_schedule/\/home\/app\/dubki\/\.bus_schedule/' route_bus.py  | tee route_bus_PA.py
 mv route_train_PA.py route_train.py
 mv route_bus_PA.py route_bus.py
 
