cf20a55e4d207ebc6d7fb774f7aa7fb01bc475e4
Max Vorobev <vmax0770@gmail.com>
2015-08-31 16:31:28 +0300

    fix deploy script and gitignore

--- PAdeploy.sh
+++ PAdeploy.sh
@@ -7,11 +7,9 @@ rsync -R *.sh *.py .train_api_key templates/* static/* .pa_temp/
 # change directory
 cd .pa_temp
 # patch the files
-sed -e 's/\.train_api_key/\/home\/dubki\/app\/\.train_api_key/' route_train.py | tee route_train_PA.py
-sed -e 's/\.bus_schedule/\/home\/dubki\/app\/\.bus_schedule/' route_bus.py  | tee route_bus_PA.py
-mv route_train_PA.py route_train.py
-mv route_bus_PA.py route_bus.py
-
+sed -i '' 's|\.bus_schedule|/home/dubki/app/\.bus_schedule|' route_bus.py
+sed -i '' 's|\.train_api_key|/home/dubki/app/\.train_api_key|' route_train.py
+sed -i '' 's|feedback.txt|/home/dubki/app/feedback.txt|' app.py
 
 LAST_COMMIT_HASH="$(git rev-parse HEAD)"
 
