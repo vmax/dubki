69c5162502fefbd45312dfccdc49438cd93f3675
Max Vorobev <vmax0770@gmail.com>
2015-09-20 18:34:33 +0300

    cache schedule for trains & ignore 'em in .gitignore

--- route_train.py
+++ route_train.py
@@ -26,14 +26,35 @@ def get_key():
 		key = F.read()
 	return key.strip()
 
-def get_nearest_train(_from, _to, _timestamp):
-	assert _from in STATIONS
-	assert _to in STATIONS
+def cache_everything():
+	from itertools import product as P
+	for (_from,_to) in P(["Одинцово"], ["Кунцево","Фили","Беговая","Белорусская"]):
+		cache_schedule(_from,_to,datetime.now())
+		cache_schedule(_to,_from,datetime.now())
 
+def cache_schedule(_from, _to,_timestamp):
 	schedule_today = json.loads(urllib.request.urlopen(form_api_url(_from, _to,_timestamp)).read().decode())
 	schedule_tomorrow = json.loads(urllib.request.urlopen(form_api_url(_from, _to,_timestamp+timedelta(days=1))).read().decode())
 
 	schedule = schedule_today['threads'] + schedule_tomorrow['threads']
+
+	with open('train_cached_%s_%s' % (STATIONS[_from], STATIONS[_to]),'w') as F:
+		json.dump(schedule, F)
+
+def get_schedule (_from, _to,_timestamp):
+	try:
+		F = open('train_cached_%s_%s' % (STATIONS[_from], STATIONS[_to]))
+		return json.load(F)
+	except FileNotFoundError:
+		cache_schedule(_from,_to, _timestamp)
+		return get_schedule(_from,_to, _timestamp)
+
+
+def get_nearest_train(_from, _to, _timestamp):
+	assert _from in STATIONS
+	assert _to in STATIONS
+
+	schedule = get_schedule(_from,_to,_timestamp)
 
 	trains = list()
 	for train in schedule:
