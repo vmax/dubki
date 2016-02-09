0a8f14dab8448f3de69d9b9038d6c77c45fdc5c8
Max Vorobev <vmax0770@gmail.com>
2016-02-09 15:54:04 +0300

    manual updating of bus schedule

--- update-schedules.sh
+++ update-schedules.sh
@@ -3,7 +3,8 @@
 #PYTHONANYWHERE
 
 mkdir -p cache
-echo "[0]: caching bus schedule"
-python3 ./route_bus.py
+# echo "[0]: caching bus schedule"
+# python3 ./route_bus.py
+echo "[0]: skipping the bus schedule update; it's now manual"
 echo "[1]: caching train schedule"
 python3 -c "from route_train import cache_everything;cache_everything()"
